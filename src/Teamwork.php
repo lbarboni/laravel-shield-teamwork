53e8312195bafa2cdac850b0236624c2a4dd2ad2
Luca Barboni <luca.barboni@essesolutions.it>
2018-10-31 17:24:06 +0100

    fix repository master

---
+++ src/Teamwork.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Shield\Teamwork;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Shield\Shield\Contracts\Service;
+
+/**
+ * Class Teamwork
+ *
+ * @package \Shield\Teamwork
+ */
+class Teamwork implements Service
+{
+    public function verify(Request $request, Collection $config): bool
+    {
+
+        if ( $config->get('disable_verify', false) ) {
+            return true;
+        }
+        $generatedHash = hash_hmac('sha256', $request->getContent(), $config->get('token'));
+        return hash_equals($generatedHash, $request->header('X-Projects-Signature'));
+    }
+    public function headers(): array
+    {
+        return ['X-Projects-Signature'];
+    }
+}
